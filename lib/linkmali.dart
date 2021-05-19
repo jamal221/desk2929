43d9c903960b737f90efc17b719d7e56847a5ac5
jamal azizbeigi <jamal.azizbeigi589@gmail.com>
2021-05-19 10:52:00 +0430

    create malilink page

--- lib/linkmali.dart
+++ lib/linkmali.dart
@@ -1,58 +1,63 @@
 import 'package:flutter/material.dart';
 import 'dart:convert';
 import 'package:http/http.dart' as http;
+import 'test123.dart';
+import 'package:url_launcher/url_launcher.dart';
+test123 t=new test123();
 
-void main() => runApp(MyApp());
-
-class MyApp extends StatelessWidget {
+class malilinks extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-            title: Text('Apply Item Filter on JSON ListView')
+            title: Padding(
+              padding: const EdgeInsets.only(left: 140),
+              child: Text('لینک های مالی',
+                ),
+            )
         ),
-        body: MainListView(),
+        body: MaliLinkView(),
       ),
     );
   }
 }
 
-class Studentdata {
-  int studentID;
-  String studentName;
-  int studentPhoneNumber;
-  String studentSubject;
+class linkdata {
+  int linkID;
+  String linkName;
+  int linkType;
+  String linkUrl;
 
-  Studentdata({
-    this.studentID,
-    this.studentName,
-    this.studentPhoneNumber,
-    this.studentSubject
+  linkdata({
+    this.linkID,
+    this.linkName,
+    this.linkType,
+    this.linkUrl
   });
 
-  factory Studentdata.fromJson(Map<String, dynamic> json) {
-    return Studentdata(
-        studentID: json['id'],
-        studentName: json['student_name'],
-        studentPhoneNumber: json['student_phone_number'],
-        studentSubject: json['student_class']
+  factory linkdata.fromJson(Map<String, dynamic> json) {
+    return linkdata(
+        linkID: json['id'],
+        linkName: json['name'],
+        linkType: json['type'],
+        linkUrl: json['Address']
 
     );
   }
 }
 
-class MainListView extends StatefulWidget {
+class MaliLinkView extends StatefulWidget {
 
-  MainListViewState createState() => MainListViewState();
+  MaliLinkViewState createState() => MaliLinkViewState();
 
 }
 
-class MainListViewState extends State {
+class MaliLinkViewState extends State {
 
-  final String apiURL = 'https://flutter-examples.000webhostapp.com/getStudentInfo.php';
+  final String apiURL = t.getaddress() +'LinksMali.php';
 
-  Future<List<Studentdata>> fetchStudents() async {
+  Future<List<linkdata>> fetchlinks() async {
 
     var response = await http.get(apiURL);
 
@@ -60,31 +65,24 @@ class MainListViewState extends State {
 
       final items = json.decode(response.body).cast<Map<String, dynamic>>();
 
-      List<Studentdata> studentList = items.map<Studentdata>((json) {
-        return Studentdata.fromJson(json);
+      List<linkdata> linkList = items.map<linkdata>((json) {
+        return linkdata.fromJson(json);
       }).toList();
 
-      return studentList;
+      return linkList;
     }
     else {
       throw Exception('Failed to load data from Server.');
     }
   }
-
-  navigateToNextActivity(BuildContext context, int dataHolder) {
-
-    Navigator.of(context).push(
-        MaterialPageRoute(
-            builder: (context) => SecondScreenState(dataHolder.toString())
-        )
-    );
-
-  }
-
   @override
   Widget build(BuildContext context) {
-    return FutureBuilder<List<Studentdata>>(
-      future: fetchStudents(),
+    return Directionality(
+      textDirection: TextDirection.rtl,
+      child: Scaffold(
+
+      body: FutureBuilder<List<linkdata>>(
+      future: fetchlinks(),
       builder: (context, snapshot) {
 
         if (!snapshot.hasData) return Center(
@@ -95,14 +93,18 @@ class MainListViewState extends State {
           children: snapshot.data
               .map((data) => Column(children: <Widget>[
             GestureDetector(
-              onTap: (){navigateToNextActivity(context, data.studentID);},
+              onTap: ()
+              {
+                //navigateToNextActivity(context, data.linkID);
+                html.window.open(data.linkUrl, data.linkName);
+                },
               child: Row(
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
 
                     Padding(
-                        padding: EdgeInsets.fromLTRB(20, 5, 0, 5),
-                        child: Text(data.studentName,
+                        padding: EdgeInsets.fromLTRB(20, 5, 20, 5),
+                        child: Text(data.linkName,
                             style: TextStyle(fontSize: 21),
                             textAlign: TextAlign.left))
 
@@ -113,105 +115,11 @@ class MainListViewState extends State {
               .toList(),
         );
       },
+    )
+    )
     );
   }
 }
 
-class SecondScreenState extends StatefulWidget {
-  final String idHolder;
-  SecondScreenState(this.idHolder);
-  @override
-  State<StatefulWidget> createState() {
-    return SecondScreen(this.idHolder);
-  }
-}
 
-class SecondScreen extends State<SecondScreenState> {
 
-  final String idHolder ;
-
-  SecondScreen(this.idHolder);
-
-  // API URL
-  var url = 'https://flutter-examples.000webhostapp.com/filter/GetStudent.php';
-
-  Future<List<Studentdata>> fetchStudent() async {
-
-    var data = {'id': int.parse(idHolder)};
-
-    var response = await http.post(url, body: json.encode(data));
-
-    if (response.statusCode == 200) {
-
-      print(response.statusCode);
-
-      final items = json.decode(response.body).cast<Map<String, dynamic>>();
-
-      List<Studentdata> studentList = items.map<Studentdata>((json) {
-        return Studentdata.fromJson(json);
-      }).toList();
-
-      return studentList;
-    }
-    else {
-      throw Exception('Failed to load data from Server.');
-    }
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-        home: Scaffold(
-            appBar: AppBar(
-                title: Text('Showing Selected Item Details'),
-                automaticallyImplyLeading: true,
-                leading: IconButton(icon:Icon(Icons.arrow_back),
-                  onPressed:() => Navigator.pop(context, false),
-                )
-            ),
-            body: FutureBuilder<List<Studentdata>>(
-              future: fetchStudent(),
-              builder: (context, snapshot) {
-
-                if (!snapshot.hasData) return Center(
-                    child: CircularProgressIndicator()
-                );
-
-                return ListView(
-                  children: snapshot.data
-                      .map((data) => Column(children: <Widget>[
-                    GestureDetector(
-                      onTap: (){print(data.studentName);},
-                      child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-
-                            Padding(
-                                padding: EdgeInsets.fromLTRB(0, 20, 0, 10),
-                                child: Text('ID = ' + data.studentID.toString(),
-                                    style: TextStyle(fontSize: 21))),
-
-                            Padding(
-                                padding: EdgeInsets.fromLTRB(0, 0, 0, 10),
-                                child: Text('Name = ' + data.studentName,
-                                    style: TextStyle(fontSize: 21))),
-
-                            Padding(
-                                padding: EdgeInsets.fromLTRB(0, 0, 0, 10),
-                                child: Text('Phone Number = ' + data.studentPhoneNumber.toString(),
-                                    style: TextStyle(fontSize: 21))),
-
-                            Padding(
-                                padding: EdgeInsets.fromLTRB(0, 0, 0, 10),
-                                child: Text('Class = ' + data.studentSubject,
-                                    style: TextStyle(fontSize: 21))),
-
-                          ]),)
-                  ],))
-                      .toList(),
-                );
-              },
-            )
-        ));
-  }
-}
