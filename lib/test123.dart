0773566edc77c691c0bb78557d79138360cb4f24
jamal azizbeigi <jamal.azizbeigi589@gmail.com>
2022-04-03 06:01:36 +0430

    Update test123.dart
    remove the original IP

--- lib/test123.dart
+++ lib/test123.dart
@@ -3,9 +3,7 @@ class test123
   String ip123;
   test123()
   {
-    ip123="http://www.desk2929.ir/app_files/";
-    //ip123="http://192.168.33.4/desk2929/";
-
+// Insert the address of your php file here
   }
   String getaddress()
   {
